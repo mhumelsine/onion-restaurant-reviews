b5ccd71aca834b0fe2f5fd1e3918df3ce685fce9
Michael Humelsine <mhumelsine@ISF.COM>
2017-10-20 13:02:11 -0400

    Added factory to infrastructure

---
+++ RestaurantReviews/RestaurantReviews.Infrastructure/Factories/RestaurantManagerFactory.cs
@@ -0,0 +1,29 @@
+﻿using Isf.XC.Validation;
+using RestaurantReviews.Core.Logic.Managers;
+using RestaurantReviews.Core.Logic.Validations;
+using RestaurantReviews.Core.Stores;
+using RestaurantReviews.Infrastructure.Stores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantReviews.Infrastructure.Factories
+{
+    public interface IRestaurantManagerFactory
+    {
+        RestaurantManager CreateInstance();
+    }
+    public class RestaurantManagerFactory : IRestaurantManagerFactory
+    {
+        public RestaurantManager CreateInstance()
+        {
+            IRestaurantStore restaurantStore = new EfRestaurantStore();
+
+            IValidator canAddRestaurantValidator = new ValidationManager(true,                
+                new DataAnnotationsValidator(),
+                new NoDuplicateRestaurantNames(restaurantStore));
+
+            return new RestaurantManager(canAddRestaurantValidator, restaurantStore);
+        }
+    }
+}
---
+++ RestaurantReviews/RestaurantReviews.Infrastructure/Stores/EfRestaurantStore.cs
@@ -0,0 +1,42 @@
+﻿using RestaurantReviews.Core.Stores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Isf.XC;
+using RestaurantReviews.Core.DTOs;
+
+namespace RestaurantReviews.Infrastructure.Stores
+{
+    public class EfRestaurantStore : IRestaurantStore
+    {
+        public void Add(Restaurant entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CommandResult CommitChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> Find(RestaurantSearch search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant Find(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Restaurant entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
